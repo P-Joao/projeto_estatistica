bf9e0b7476d5b46b25444984412631bae6a59f7a
P-Joao <rjoaopedro2020@gmail.com>
2025-05-30 19:49:50 -0300

    Primeiro commit

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+st.title("Estatística e Probabilidade")
+st.write("Projeto de estatística voltado para análise exploratória de bases de dados relacionadas a ocorrências de crimes na cidade de Los Angeles, CA a partir do ano de 2020.")
+
+bases, analises, relatorio = st.columns(3)
+with bases.container(border=True):
+    st.button(label=":material/database: Docs e Bases")
+    st.write("Bases, bibliotecas e tecnologias utilizadas no projeto.")
+with analises.container(border=True):
+    st.button(label=":material/edit_note: Análises Realizadas")
+    st.write("Análises exploratórias realizadas, questionamentos e cálculos estatísticos.")
+with relatorio.container(border=True):
+    st.button(label=":material/newspaper: Relatório Final")
+    st.write("Correlações, informações e conclusões encontradas no decorrer do projeto.")
+
+st.title("Integrantes do Grupo")
+beatriz, felipe, joao = st.columns(3)
+with beatriz.container(border=True):
+    st.header("Beatriz", divider=True)
+with felipe.container(border=True):
+    st.header("Felipe", divider=True)
+with joao.container(border=True):
+    st.header("João", divider=True)
+    st.subheader("SP3137627")
+    linkedin_url_joao = "https://www.linkedin.com/in/j-pedro-rodrigues/"
+    github_url_joao = "https://github.com/P-Joao"
+    st.markdown(f"[:material/open_in_new: LinkedIn]({linkedin_url_joao}) &nbsp;&nbsp; [:material/open_in_new: GitHub]({github_url_joao})")
