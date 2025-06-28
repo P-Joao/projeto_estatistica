04d4f8b6e17be7be11ce80bf9acd54a0d00b7410
P-Joao <rjoaopedro2020@gmail.com>
2025-06-28 13:18:15 -0300

    Início das análises na base de prisões

--- pages/Analises.py
+++ pages/Analises.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(
     page_title="Análises Realizadas",
@@ -6,3 +7,33 @@ st.set_page_config(
 )
 
 st.title("Análises Realizadas")
+
+st.header("Base de prisões de 2020 para o presente")
+df_arrest_data = pd.read_csv('mock_data\Arrest_Data_from_2020_to_Present.csv')
+
+st.header(f"Colunas")
+
+
+st.write(f"Quantidade de registros: ", len(df_arrest_data))
+
+st.header("Características dos suspeitos presos")
+df_suspeitos = df_arrest_data[['Age', 'Sex Code', 'Descent Code']]
+
+st.write('Realizar testes de hipotese para verificar se diferença de proporção entre descendencias')
+
+st.write(df_suspeitos)
+colAge, colSex, colDescent = st.columns(3)
+colAge.write(df_suspeitos['Age'].describe())
+colSex.write(df_suspeitos['Sex Code'].describe())
+colDescent.write(df_suspeitos['Descent Code'].describe())
+
+st.write('''
+    Abaixo de 10 anos não há registros de "crime" na descrição, ou são "não criminais" ou nulos.
+    Alguns podem ser registros errados. 
+''')
+df_menores_10 = df_arrest_data[['Age', 'Charge Group Description']].loc[df_arrest_data['Age'] < 10].drop_duplicates()
+df_menores_10
+
+
+
+df_arrest_data
